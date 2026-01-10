a463d1572f45fc3db018653421ebc9d7f4ee01bb
DaniilZH1 <stalin.garvild@gmail.com>
2026-01-10 15:16:07 +0500

    lab

---
+++ lb7/src/Timus/Timus_1133.java
@@ -0,0 +1,70 @@
+package Timus;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Timus_1133 {
+    public static void main(String[] args) {
+
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int i   = sc.nextInt();
+        long Fi = sc.nextLong();
+        int j   = sc.nextInt();
+        long Fj = sc.nextLong();
+
+        int k   = sc.nextInt();
+
+
+
+        if (i == k) {
+            System.out.println(Fi);
+            System.exit(0);
+        } else if (j == k) {
+            System.out.println(Fj);
+            System.exit(0);
+        }
+
+
+        if (j < i) {
+            int itmp = i;
+            i   = j;
+            j   = itmp;
+            long tmp = Fi;
+            Fi  = Fj;
+            Fj  = tmp;
+        }
+
+        BigInteger fib[] = new BigInteger[2001];
+        fib[0] = new BigInteger("0");
+        fib[1] = new BigInteger("1");
+        for (int h = 2; h <= 2000; h++) {
+            fib[h] = fib[h - 1].add(fib[h - 2]);
+        }
+
+        BigInteger FI = new BigInteger(String.valueOf(Fi));
+        BigInteger FJ = new BigInteger(String.valueOf(Fj));
+
+        BigInteger Fip = FJ.subtract(fib[j - i - 1].multiply(FI)).divide(fib[j - i]);
+        BigInteger FK  = Fip;
+
+        if (k < i) {
+            while (--i != k) {
+                FK  = Fip.subtract(FI);
+                Fip = FI;
+                FI  = FK;
+            }
+        } else {
+            while (++i != k) {
+                FK  = Fip.add(FI);
+                FI  = Fip;
+                Fip = FK;
+            }
+        }
+
+        System.out.println(FK);
+
+        sc.close();
+    }
+}
