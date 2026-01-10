f04974fc1e5204516767ab67859820d056346df3
DaniilZH1 <stalin.garvild@gmail.com>
2026-01-10 16:57:29 +0500

    lab

---
+++ src/Timus/T1023.java
@@ -0,0 +1,18 @@
+package Timus;
+import java.util.Scanner;
+public class T1023 {
+    public static void main(String[] args) {
+
+        long i = 3;
+        long a;
+
+
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextLong();
+        while (a % i != 0) {
+            i++;
+        }
+
+        System.out.println(i - 1);
+    }
+}
